c5131e325274efe101c6675096db12156e22c143
Nicolas Scozzaro <nscozzaro@gmail.com>
2017-01-24 18:16:14 -0500

    Bokeh plot working, but not feature checkboxes

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import simplejson as json
+import pandas as pd
+from bokeh.plotting import figure
+from bokeh.embed import components
+
 
 app = Flask(__name__)
 
@@ -16,12 +20,25 @@ def index():
 def graph():
     form_dict = {}
     if request.method == 'POST':
+        # Extract form info
         ticker = request.form['ticker']
+        features = request.form.getlist('features')
+
+        # Call the Quandl API
         api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=bhDkb5WTxo-gXcFN5mgq' % ticker
         response = requests.get(api_url)
         json_response = json.loads(response.content)
-        return render_template('graph.html', response=json_response['id'])
+
+        # Put the data in a Pandas dataframe
+        data = pd.DataFrame(json_response['data'], columns=json_response['column_names'])
+        data['Date'] = pd.to_datetime(data['Date'])
+
+        # Send the data to the Bokeh plot
+        p = figure(x_axis_type="datetime", width=800, height=600)
+        p.line(data['Date'], data['Close'])
+        script, div = components(p)
+        return render_template('graph.html', script=script, div=div)
 
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=5000)
