9bcf567dee8bf9a048f3603d13d2c476bc7d4b5e
Nicolas Scozzaro <nscozzaro@gmail.com>
2017-01-24 10:54:30 -0500

    Getting form and API query working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
@@ -12,9 +13,12 @@ def index():
 
 @app.route('/graph', methods=["GET", "POST"])
 def graph():
+    form_dict = {}
     if request.method == 'POST':
-        data = request.form
-        return render_template('graph.html', data = data)
+        data = request.form['ticker']
+        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=gVz7XbzeecyxHdkCn8yB' % 'ticker'
+        response = requests.get(api_url)
+        return render_template('graph.html', data = data, response=response.content)
 
 
 if __name__ == '__main__':
